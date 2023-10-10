d9e2d162f2b7fc49ad328f2cf24e10b65a45bad5
Lazare <lazare.carpier.com>
2023-10-10 15:18:29 +0200

    React stamped

---
+++ React/ex10/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import Menu from "./Menus"
+
+const Home = () =>{
+
+    return (
+        <>
+        <Menu/>
+        <main className="card dark">
+            <h3 className="display-4">Accueil</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque optio totam doloribus aut numquam quaerat quidem voluptatum minus, cupiditate sequi error eum iure delectus deleniti autem, nesciunt facere? Perspiciatis, error. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium corporis voluptatibus voluptas libero, repellendus dolorem tempore nisi vel, nulla ad fugiat? Impedit aut culpa ullam itaque dicta deleniti at nemo! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, laboriosam, minima consequatur nesciunt vero, rem iste maxime provident cum obcaecati nobis sint corrupti! Saepe labore alias explicabo nesciunt delectus libero. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eum fugit esse. Ea, laborum blanditiis. Reiciendis dicta architecto nihil omnis quisquam eveniet aliquam, aut doloremque rerum id praesentium explicabo! Minus.</p>
+        </main>
+        </>
+    )
+}
+
+export default Home
